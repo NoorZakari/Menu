1775dd463aa260c30eac0e3017acf8511f107a96
Eman&nawar <emanalisabha@gmail.com>
2022-08-13 03:31:11 -0800

    /

--- js/script.js
+++ js/script.js
@@ -1,81 +1,81 @@
-var responsiveSlider = function(){
-    var slider  =document.getElementById("slider");
-    var sliderWidth = slider.offsetWidth;
-    var slideList =document.getElementById("slideWrap");
-    var count = 1;
-    var items = slideList.querySelectorAll("li").length;
-    var prev =document.getElementById("prev");
-    var next = document.getElementById("next");
+// var responsiveSlider = function(){
+//     var slider  =document.getElementById("slider");
+//     var sliderWidth = slider.offsetWidth;
+//     var slideList =document.getElementById("slideWrap");
+//     var count = 1;
+//     var items = slideList.querySelectorAll("li").length;
+//     var prev =document.getElementById("prev");
+//     var next = document.getElementById("next");
 
-    window.addEventListener('resize' ,function(){
-sliderWidth =slider.offsetWidth;
-    });
+//     window.addEventListener('resize' ,function(){
+// sliderWidth =slider.offsetWidth;
+//     });
 
-    var prevSlide = function(){
-        if(count > 1){
-            count = count - 2 ;
-            slideList.style.left="-" +count  *  sliderWidth + "px";
-            count++;
-        }
-        else if(count = 1 ){
-            count = items -1;
-            slideList.style.left="-" +count  *  sliderWidth + "px";
-            count++;
-        }
-    };
+//     var prevSlide = function(){
+//         if(count > 1){
+//             count = count - 2 ;
+//             slideList.style.left="-" +count  *  sliderWidth + "px";
+//             count++;
+//         }
+//         else if(count = 1 ){
+//             count = items -1;
+//             slideList.style.left="-" +count  *  sliderWidth + "px";
+//             count++;
+//         }
+//     };
     
-    var nextSlide = function(){
-        if(count < items){
-            slideList.style.left="-" +count  *  sliderWidth + "px";
-            count++;
-        }
-        else if(count = items ){
+//     var nextSlide = function(){
+//         if(count < items){
+//             slideList.style.left="-" +count  *  sliderWidth + "px";
+//             count++;
+//         }
+//         else if(count = items ){
            
-            slideList.style.left="0px" ;
-            count=1;
-        }
-    };
+//             slideList.style.left="0px" ;
+//             count=1;
+//         }
+//     };
 
-    next.addEventListener("click",function(){
-        nextSlide();
-    });
+//     next.addEventListener("click",function(){
+//         nextSlide();
+//     });
 
-    prev.addEventListener("click",function(){
-       prevSlide();
-    });
-    setInterval(function() {
-    nextSlide()
-    },5000);
+//     prev.addEventListener("click",function(){
+//        prevSlide();
+//     });
+//     // setInterval(function() {
+//     // nextSlide()
+//     // },5000);
 
-}
-window.onload=function(){
-    responsiveSlider();
-};
+// }
+// window.onload=function(){
+//     responsiveSlider();
+// };
 
 // *******Touch slide
-const wrapper = document.querySelector('.Wrap')
+// const wrapper = document.querySelector('.Wrap')
 
-let pressed = false
-let startX = 0
+// let pressed = false
+// let startX = 0
 
-wrapper.addEventListener('mousedown', function (e) {
-  pressed = true
-  startX = e.clientX
-  this.style.cursor = 'grabbing'
-})
+// wrapper.addEventListener('mousedown', function (e) {
+//   pressed = true
+//   startX = e.clientX
+//   this.style.cursor = 'grabbing'
+// })
 
-wrapper.addEventListener('mouseleave', function (e) {
-  pressed = false
-})
+// wrapper.addEventListener('mouseleave', function (e) {
+//   pressed = false
+// })
 
-window.addEventListener('mouseup', function (e) {
-  pressed = false
-  wrapper.style.cursor = 'grab'
-})
+// window.addEventListener('mouseup', function (e) {
+//   pressed = false
+//   wrapper.style.cursor = 'grab'
+// })
 
-wrapper.addEventListener('mousemove', function (e) {
-  if(!pressed) {
-    return
-  }
-  this.scrollLeft += startX - e.clientX
-})
+// wrapper.addEventListener('mousemove', function (e) {
+//   if(!pressed) {
+//     return
+//   }
+//   this.scrollLeft += startX - e.clientX
+// })
